d274697c9cbccd516a8555ad60f2e805fa972502
sh4mia <sobalkowski@gmail.com>
2023-06-25 15:13:16 +0200

    update 1

--- zad1.sh
+++ zad1.sh
@@ -1,4 +1,15 @@
-if [ "$1" = "--logs" ]; then
+#!/bin/bash
+
+show_help() {
+    echo "Dostępne opcje:"
+    echo "    --logs, -l - utworzenie plików log"
+    echo "    --logs [liczba], -l [liczba] - utworzenie określonej liczby plików log"
+    echo "    --date, -d - wyświetlenie dzisiejszej daty"
+    echo "    --help, -h - wyświetlenie dostępnych opcji"
+    echo "    -a - dodatkowe flagi"
+}
+
+if [[ "$1" = "--logs" ]] || [[ "$1" = "-l" ]]; then
     if [ -n "$2" ]; then
         for ((i=1; i<=$2; i++)); do
             echo "Nazwa pliku: log$i.txt" > log$i.txt
@@ -8,14 +19,19 @@ if [ "$1" = "--logs" ]; then
     else
         echo "Podaj liczbę plików do utworzenia."
     fi
-elif [ "$1" = "--help" ]; then
-    echo "Dostępne opcje:"
-    echo "    --logs - utworzenie plików log"
-    echo "    --logs [liczba] - utworzenie określonej liczby plików log"
-    echo "    --date - wyświetlenie dzisiejszej daty"
-    echo "    --help - wyświetlenie dostępnych opcji"
-elif [ "$1" = "--date" ]; then
+elif [[ "$1" = "--help" ]] || [[ "$1" = "-h" ]]; then
+    if [[ "$2" = "-a" ]]; then
+        show_help
+        echo "Dodatkowe flagi:"
+        echo "    -f - flaga F"
+        echo "    -g - flaga G"
+    else
+        show_help
+    fi
+elif [[ "$1" = "--date" ]] || [[ "$1" = "-d" ]]; then
     echo "Dzisiejsza data: $(date)"
 else
     echo "Nieznana opcja. Użyj --help, aby wyświetlić dostępne opcje."
 fi
+
+
