468c83d39f1e31bce50f3429d24370cef8a0473d
sh4mia <sobalkowski@gmail.com>
2023-06-25 15:17:16 +0200

    update 5

--- zad1.sh
+++ zad1.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 show_help() {
     echo "Dostępne opcje:"
     echo "    --logs, -l - utworzenie plików log"
@@ -13,19 +11,34 @@ show_help() {
 if [[ "$1" = "--logs" ]] || [[ "$1" = "-l" ]]; then
     if [ -n "$2" ]; then
         for ((i=1; i<=$2; i++)); do
-	    mkdir log$i
+            mkdir log$i
             echo "Nazwa pliku: log$i.txt" > log$i/log$i.txt
             echo "Skrypt: skrypt.sh" >> log$i/log$i.txt
             echo "Data: $(date)" >> log$i/log$i.txt
         done
-    
     else
         for ((i=1; i<=100; i++)); do
-	    mkdir log$i
-	    echo "Nazwa pliku: log$i.txt" > log$i/log$i.txt
-	    echo "Skrypt: skrypt.sh" >> log$i/log$i.txt
-	    echo "Data: $(date)" >> log$i/log$1.txt
-	   done
+            mkdir log$i
+            echo "Nazwa pliku: log$i.txt" > log$i/log$i.txt
+            echo "Skrypt: skrypt.sh" >> log$i/log$i.txt
+            echo "Data: $(date)" >> log$i/log$i.txt
+        done
+    fi
+elif [[ "$1" = "--error" ]] || [[ "$1" = "-e" ]]; then
+    if [ -n "$2" ]; then
+        for ((i=1; i<=$2; i++)); do
+            mkdir error$i
+            echo "Nazwa pliku: error$i.txt" > error$i/error$i.txt
+            echo "Skrypt: skrypt.sh" >> error$i/error$i.txt
+            echo "Data: $(date)" >> error$i/error$i.txt
+        done
+    else
+        for ((i=1; i<=100; i++)); do
+            mkdir error$i
+            echo "Nazwa pliku: error$i.txt" > error$i/error$i.txt
+            echo "Skrypt: skrypt.sh" >> error$i/error$i.txt
+            echo "Data: $(date)" >> error$i/error$i.txt
+        done
     fi
 elif [[ "$1" = "--help" ]] || [[ "$1" = "-h" ]]; then
     if [[ "$2" = "-a" ]]; then
@@ -42,6 +55,13 @@ elif [[ "$1" = "--init" ]]; then
     git clone <repozytorium> .
     export PATH=$(pwd):$PATH
     echo "Repozytorium zostało sklonowane i ścieżka została ustawiona w PATH."
+elif [[ "$1" = "skrypt.sh" ]] && [[ "$2" = "-e" ]] && [[ -n "$3" ]]; then
+    for ((i=1; i<=$3; i++)); do
+        mkdir error$i
+        echo "Nazwa pliku: error$i.txt" > error$i/error$i.txt
+        echo "Skrypt: skrypt.sh" >> error$i/error$i.txt
+        echo "Data: $(date)" >> error$i/error$i.txt
+    done
 else
     echo "Nieznana opcja. Użyj --help, aby wyświetlić dostępne opcje."
 fi
